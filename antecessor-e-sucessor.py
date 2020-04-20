eac00f7282319274aa936c2821792c3cb6875494
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-04-20 17:01:27 -0300

    Create antecessor-e-sucessor.py

---
+++ antecessor-e-sucessor.py
@@ -0,0 +1,6 @@
+https://www.youtube.com/watch?v=664e0G_S9nU&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=5
+
+n = int (input('Digite um número: ')
+a = n - 1
+s = n + 1 
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, a, s))
