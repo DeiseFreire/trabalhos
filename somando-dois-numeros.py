27adcb7017508ac138f6320b71b1f400c3e431ea
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-04-23 12:46:38 -0300

    Update somando-dois-numeros.py

--- somando-dois-numeros.py
+++ somando-dois-numeros.py
@@ -1,4 +1,4 @@
-https://www.youtube.com/watch?v=PB254Cfjlyk&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=3
+# https://www.youtube.com/watch?v=PB254Cfjlyk&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=3
 
 n1 = int (input ('Digite um valor: '))
 n2 = int (input ('Digite outro valor: '))
