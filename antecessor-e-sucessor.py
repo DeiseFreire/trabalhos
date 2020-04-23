149193d873e578959bd91e3ab24033133f89f9ab
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-04-23 12:45:15 -0300

    Update antecessor-e-sucessor.py

--- antecessor-e-sucessor.py
+++ antecessor-e-sucessor.py
@@ -1,4 +1,4 @@
-https://www.youtube.com/watch?v=664e0G_S9nU&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=5
+# https://www.youtube.com/watch?v=664e0G_S9nU&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=5
 
 n = int (input('Digite um número: ')
 a = n - 1
