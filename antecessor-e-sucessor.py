e2c3c4ef77aa13512f8e5431728c9438b9c6b8a9
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-06-09 12:23:46 -0300

    Update antecessor-e-sucessor.py

--- antecessor-e-sucessor.py
+++ antecessor-e-sucessor.py
@@ -1,3 +1,4 @@
+# Exercício Python #005 - Antecessor e Sucessor
 # https://www.youtube.com/watch?v=664e0G_S9nU&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=5
 
 n = int (input('Digite um número: ')
