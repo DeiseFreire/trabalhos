9d1bf6e3bce3c653c0abd47d65210a5c81f25610
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-07-15 09:24:58 -0300

    Create Conversor-de-Moedas.py

---
+++ Conversor-de-Moedas.py
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=xM4AX3Lp2mo&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=11
+
+real = float (input('Quanto dinheiro você tem na carteira? '))
+dolar = real / 3.27
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar)) 
