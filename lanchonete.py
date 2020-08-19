f5459ee9ca67c7867be29cb31973df280376273f
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-08-18 22:03:55 -0300

    Update lanchonete.py

--- lanchonete.py
+++ lanchonete.py
@@ -16,7 +16,7 @@ framef2.pack(side=RIGHT)
 # FRAME SECUNDÁRIOS
 frameft2 = Frame(framef2, width=440, height=450, bd=2, relief="raise")
 frameft2.pack(side=TOP)
-framefb2 = Frame(framef2, width=440, height=250, bd=2, relief="raise")
+framefb2 = Frame(framef2, width=440, height=250, bg=’black’, bd=2, relief="raise")
 framefb2.pack(side=BUTTON)
 framef1a = Frame(framef1a, width=900, height=330, bd=2, relief="raise")
 framef1a.pack(side=TOP)
@@ -35,29 +35,29 @@ frameRODAPE = Frame(framef2a, width=211, height=190, bd=0, relief="raise")
 frameRODAPE.pack(side=BUTTON)
 # TROCANDO A COR DE FUNDO DO FRAME DA ESQUERDA
 framef1a.configure(background
-'grey')
+'gray')
 # INSERINDO O RÓTULO DO CABECALHO COM O TÍTULO
-lblInfo = tkinter.Label(Tops, font=('arial', 70, 'bold'), text="SISTEMA CAFETERIA", bd=8, width=23)
+lblInfo = Label(Tops, font=('arial', 70, 'bold'), text="SISTEMA CAFETERIA", bd=8, width=23)
 lblInfo =.grid(row=0, column=0)
 # DECLARANDO AS PRIMEIRAS VARIÁVEIS
-var1 = tkinter.IntVar()
-var2 = tkinter.IntVar()
-var3 = tkinter.IntVar()
-var4 = tkinter.IntVar()
-var5 = tkinter.IntVar()
-var6 = tkinter.IntVar()
-var7 = tkinter.IntVar()
-var8 = tkinter.IntVar()
-var9 = tkinter.IntVar()
-var10 = tkinter.IntVar()
-var11 = tkinter.IntVar()
-var12 = tkinter.IntVar()
-var13 = tkinter.IntVar()
-var14 = tkinter.IntVar()
-var15 = tkinter.IntVar()
-var16 = tkinter.IntVar()
-var17 = tkinter.IntVar()
-var18 = tkinter.IntVar()
+var1 = IntVar()
+var2 = IntVar()
+var3 = ntVar()
+var4 = IntVar()
+var5 = IntVar()
+var6 = IntVar()
+var7 = IntVar()
+var8 = IntVar()
+var9 = IntVar()
+var10 = IntVar()
+var11 = IntVar()
+var12 = IntVar()
+var13 = IntVar()
+var14 = IntVar()
+var15 = IntVar()
+var16 = IntVar()
+var17 = IntVar()
+var18 = IntVar()
 # SETANDO AS VARIÁVEIS
 var1.set("0")
 var2.set("0")
@@ -78,63 +78,54 @@ var16.set("0")
 var17.set("0")
 var18.set("0")
 # CRIANDO OS RADIO BUTTONS PARA O CAFE
-Cafe_comLeite = tkinter.Checkbutton(framef1aa, text=”Cafe_comLeite \t
-", variable = var1, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=0,sticky = W)
-Expresso = tkinter.Checkbutton(framef1aa, text=”Expresso \t
-", variable = var2, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=1,sticky = W)
-Cafe_comChantilly = tkinter.Checkbutton(framef1aa, text=”Cafe_comChantilly \t
-", variable = var3, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=2,sticky = W)
-Cafe_Gelado = tkinter.Checkbutton(framef1aa, text=”Cafe_Gelado \t
-", variable = var4, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=2,sticky = W)
-Cafe_Cremoso = tkinter.Checkbutton(framef1aa, text=”Cafe_Cremoso  \t
+Cafe_comLeite = Checkbutton(framef1aa, text=”Cafe_comLeite \t
+", variable = var1, onvalue = 1, offvalue = 0, width=20, bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0,sticky = W)
+Expresso = Checkbutton(framef1aa, text=”Expresso \t
+", variable = var2, onvalue = 1, offvalue = 0, width=20,bg='DarkSeaGreen3',fg='blue', font=('arial', 18, 'bold')).grid(row=1,sticky = W)
+Cafe_comChantilly = Checkbutton(framef1aa, text=”Cafe_comChantilly \t
+", variable = var3, onvalue = 1, offvalue = 0, width=20,bg='DarkSeaGreen3',fg='blue', font=('arial', 18, 'bold')).grid(row=2,sticky = W)
+Cafe_Gelado = Checkbutton(framef1aa, text=”Cafe_Gelado \t
+", variable = var4, onvalue = 1, offvalue = 0, width=20,bg='DarkSeaGreen3',fg='blue', font=('arial', 18, 'bold')).grid(row=2,sticky = W)
+Cafe_Cremoso = Checkbutton(framef1aa, text=”Cafe_Cremoso  \t
 ", variable = var5, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=3,sticky = W)
-Cafe_comChocolate = tkinter.Checkbutton(framef1aa, text=”Cafe_comChocolate \t
-", variable = var6, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=4,sticky = W)
-Cafe_Preto = tkinter.Checkbutton(framef1aa, text=”Cafe_Preto \t
-", variable = var7, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=5,sticky = W)
-Cafe_Creme = tkinter.Checkbutton(framef1aa, text=”Cafe_Creme \t
-", variable = var8, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=6,sticky = W)
-Cappuccino = tkinter.Checkbutton(framef1aa, text=”Cappuccino \t
-", variable = var9, onvalue = 1, offvalue = 0, width=20, font=('arial', 18, 'bold')).grid(row=7,sticky = W)
+Cafe_comChocolate = Checkbutton(framef1aa, text=”Cafe_comChocolate \t
+", variable = var6, onvalue = 1, offvalue = 0, width=20,bg='DarkSeaGreen3',fg='blue', font=('arial', 18, 'bold')).grid(row=4,sticky = W)
+Cafe_Preto = Checkbutton(framef1aa, text=”Cafe_Preto \t
+", variable = var7, onvalue = 1, offvalue = 0, width=20,bg='DarkSeaGreen3',fg='blue', font=('arial', 18, 'bold')).grid(row=5,sticky = W)
+Cafe_Creme =Checkbutton(framef1aa, text=”Cafe_Creme \t
+", variable = var8, onvalue = 1, offvalue = 0, width=20,bg='DarkSeaGreen3',fg='blue', font=('arial', 18, 'bold')).grid(row=6,sticky = W)
+Cappuccino = Checkbutton(framef1aa, text=”Cappuccino \t
+", variable = var9, onvalue = 1, offvalue = 0, width=20,bg='DarkSeaGreen3',fg='blue', font=('arial', 18, 'bold')).grid(row=7,sticky = W)
 # CRIANDO OS RADIO BUTTONS PARA OS BOLOS
 Bolo_Limao = Checkbutton(framef1ab, text="Bolo_Limao \t", variable=var9, onvalue=1, offvalue=0,
-                        font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Bolo_Milho = Checkbutton(framef1ab, text="Bolo_Milho \t", variable=var10, onvalue=1, offvalue=0,
-                        font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Bolo_Morando = Checkbutton(framef1ab, text="Bolo_Morando \t", variable=var11, onvalue=1, offvalue=0,
-                          font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Torta = Checkbutton(framef1ab, text="Torta \t", variable=var12, onvalue=1, offvalue=0, font=('arial', 18, 'bold')).grid(
-   row=0, sticky=W)
-Bolo_Cenoura = Checkbutton(framef1ab, text="Bolo_Cenoura \t", variable=var13, onvalue=1, offvalue=0,
-                          font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Bolo_Chocolate = Checkbutton(framef1ab, text="Bolo_Chocolate \t", variable=var14, onvalue=1, offvalue=0,
-                            font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Bolo_Prestigio = Checkbutton(framef1aa, text="Bolo_Prestigio \t", variable=var15, onvalue=1, offvalue=0,
-                            font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Bolo_FlorestaNegra = Checkbutton(framef1ab, text="Bolo_FlorestaNegra \t", variable=var16, onvalue=1, offvalue=0,
-                                font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Bolo_Pamonha = Checkbutton(framef1ab, text="Bolo_Pamonha \t", variable=var17, onvalue=1, offvalue=0,
-                          font=('arial', 18, 'bold')).grid(row=0, sticky=W)
-Bolo_SonhoDeValsa = Checkbutton(framef1ab, text="Bolo_SonhoDeValsa \t", variable=var18, onvalue=1, offvalue=0,
-                               font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+width=15,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_Milho = Checkbutton(framef1ab, text="Bolo_Milho \t", variable=var10, onvalue=1, offvalue=0, width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_Morando = Checkbutton(framef1ab, text="Bolo_Morando \t", variable=var11, onvalue=1, offvalue=0, width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Torta = Checkbutton(framef1ab, text="Torta \t", variable=var12, onvalue=1, offvalue=0, width=13,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_Cenoura = Checkbutton(framef1ab, text="Bolo_Cenoura \t", variable=var13, onvalue=1, offvalue=0,width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_Chocolate = Checkbutton(framef1ab, text="Bolo_Chocolate \t", variable=var14, onvalue=1, offvalue=0,width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_Prestigio = Checkbutton(framef1aa, text="Bolo_Prestigio \t", variable=var15, onvalue=1, offvalue=0,width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_FlorestaNegra = Checkbutton(framef1ab, text="Bolo_FlorestaNegra \t", variable=var16, onvalue=1, offvalue=0,width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_Pamonha = Checkbutton(framef1ab, text="Bolo_Pamonha \t", variable=var17, onvalue=1, offvalue=0,width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
+Bolo_SonhoDeValsa = Checkbutton(framef1ab, text="Bolo_SonhoDeValsa \t", variable=var18, onvalue=1, offvalue=0,width=20,bg='DarkSeaGreen3',fg='blue',font=('arial', 18, 'bold')).grid(row=0, sticky=W)
 # CRIANDO OS CAMPOS DE TEXTO, CAIXA TEXTO DO CAFÉ
-txtLatte = Entry(frame1aa, font=('arial', 16, 'bold'), bd=1, width=6, justify='left', state=DISABLED)
+txtLatte = Entry(frame1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=6, justify='left', state=NORMAL)
 txtLatte.grid(row=0, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=NORMAL)
 txtLatte.grid(row=1, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=8, justify='left', state=DISABLED)
 txtLatte.grid(row=2, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=8, justify='left', state=DISABLED)
 txtLatte.grid(row=3, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=8, justify='left', state=DISABLED)
 txtLatte.grid(row=4, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=8, justify='left', state=DISABLED)
 txtLatte.grid(row=5, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=8, justify='left', state=DISABLED)
 txtLatte.grid(row=6, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=8, justify='left', state=DISABLED)
 txtLatte.grid(row=7, column=1)
-txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1, width=8, justify='left', state=DISABLED)
+txtLatte = Entry(framef1aa, font=('arial', 16, 'bold'), bd=1,bg='SKYBlue1', width=8, justify='left', state=DISABLED)
 txtLatte.grid(row=8, column=1)
 # CRIANDO OS CAMPOS DE TEXTO, CAIXA TEXTO, DOS BOLOS
 txtLatte = Entry(frame1ab, font=('arial', 16, 'bold'), bd=1, width=6, justify='left', state=DISABLED)
@@ -157,11 +148,11 @@ txtLatte = Entry(framef1ab, font=('arial', 16, 'bold'), bd=1, width=8, justify='
 txtLatte.grid(row=8, column=1)
 # CAMPO DO RECIBO
 lblRecibo = Label = (frameft2, font=('arial', 12, 'bold'), text = "Recibo do Cliente :", bd = 2, anchor = 'W')
-lblRecibo.grid(row=0, column=0, sticky=tkinter.W)
+lblRecibo.grid(row=0, column=0, sticky=W)
 lblRecibo = Text = (frameft2, width=59, height=28, bg"white", bd3, font=('arial', 10))
 textRecibo.grid(row=1, column=0)
 # CAMPOS DOS BOTÕES
-cmdTotal = Button(framefb2, padx=16, pady=1, bg='black’, bd=4, fg='black', font=('arial', 16, 'bold'), width=5, text='Total').grid(
+cmdTotal = Button(framefb2, padx=16, pady=1, bg='black’, bd=4, fg='green', font=('arial', 16, 'bold'), width=5, text='Total').grid(
    row=0, column=0)
 cmdRecibo = Button(framefb2, padx=16, pady=1, bg='black’, bd=4, fg=’white', font=('arial', 16, 'bold'), width=5, text='Recibo').grid(
    row=0, column=1)
@@ -187,20 +178,19 @@ bd = 2, justify = 'left')
 txtTaxaservico.grid(row=2, column=1)
 # CRIANDO OS CAMPOS DO RODAPE  FRAME DA DIREITA
 lblCustobedidas = Label(framef2ab, font=('arial', 12, 'bold'), text="Imposto :", bd=2)
-lblCustobebidas.grid(row=0, column=0, sticky=tkinter.W)
-txtCustobebidas = tkinter.Entry(framef2ab, font=('arial', 16, 'bold')
+lblCustobebidas.grid(row=0, column=0, sticky=W)
+txtCustobebidas = Entry(framef2ab, font=('arial', 16, 'bold')
 bd = 2, justify = 'left')
 txtCustobebidas.grid(row=0, column=1)
 lblCustobedidas = Label(framef2ab, font=('arial', 12, 'bold'), text="Sub Total :", bd=2)
-lblCustobebidas.grid(row=1, column=0, sticky=tkinter.W)
-txtCustobebidas = tkinter.Entry(framef2aa, font=('arial', 16, 'bold')
+lblCustobebidas.grid(row=1, column=0, sticky=W)
+txtCustobebidas = Entry(framef2aa, font=('arial', 16, 'bold')
 bd = 2, justify = 'left')
 txtCustobebidas.grid(row=1, column=1)
 lblCustobedidas = Label(framef2ab, font=('arial', 12, 'bold'), text="Total  :", bd=2)
-lblCustobebidas.grid(row=2, column=0, sticky=tkinter.W)
-txtCustobebidas = tkinter.Entry(framef2ab, font=('arial', 16, 'bold')
+lblCustobebidas.grid(row=2, column=0, sticky=W)
+txtCustobebidas = Entry(framef2ab, font=('arial', 16, 'bold')
 bd = 2, justify = 'left')
 txtCustobebidas.grid(row=2, column=1)
 root.mainloop()
-
 
