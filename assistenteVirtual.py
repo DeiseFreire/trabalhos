3daaf498ba3e40d3c5afa9ef058f3b10fe649314
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-08-23 08:00:08 -0300

    Update assistenteVirtual.py

--- assistenteVirtual.py
+++ assistenteVirtual.py
@@ -6,7 +6,7 @@ import speech_recognition as sr
 # speech synthesis
 import pyttsx3
 bot=ChatBot('Logo') # inicia o bot
-chats=['hi','hello','how are you?','I"me fine.','thanks'] # conversas
+chats=["hi","hello","how are you?","I'me fine.","thanks"] # conversas
 bot.set_trainer(ListTrainer) # define o método de treinamento
 bot.train(chats) # define a lista de conversas 
 while True:
