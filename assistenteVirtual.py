dd0c43a7271f9353943e15af08c81852082cde49
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-08-28 20:13:10 -0300

    Update assistenteVirtual.py

--- assistenteVirtual.py
+++ assistenteVirtual.py
@@ -9,21 +9,21 @@ bot=ChatBot('Logo') # inicia o bot
 speak=pyttsx3.init('sapi5')
 def Speak(text): 
 speak.say(text)
-speak.runAndWait()
-chats=['hi','hello','how are you?','I"me fine.','thanks'] # conversas
+chats=['hi','hello','how are you?','I am fine.','thanks'] # conversas
 bot.set_trainer(ListTrainer) # define o método de treinamento
 bot.train(chats) # define a lista de conversas 
 r=sr.Recognizer()
 with sr.Microphone() as s:
 r.adjust_for_ambient_noise(s) # se adaptar ao ruído
-while True : 
 print('Say something: ') 
+while True : 
+try:
 audio=r.listen(s) # escutar
 speech=r.recognize_google(audio) #fala
+response=bot.get_response(speech)
 #request=input('Enter a text: ')
-print(bot.get_response(speech))
 print('You: ',speech) 
-print('Bot: ',response) 
-Speak(response) 
-#print('Bot :',bot.get_response(speech))   
-
+print('Bot: ',response)
+Speak(response)
+except:
+print('Sorry. ') 
