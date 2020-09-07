e4b1b29edc606056e2cc6690c3d1c23f0cab74d9
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-09-07 09:05:33 -0300

    Create CadastrodeTrabalhadoremPython.py

---
+++ CadastrodeTrabalhadoremPython.py
@@ -0,0 +1,18 @@
+# Exercício Python #092 - Cadastro de Trabalhador em Python
+# https://www.youtube.com/watch?v=Vsqemzdrj78&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=93
+
+from datetime import datetime
+
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de Nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de Contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+    print('-=' * 30)
+for k, v in dados.items():
+    print(f' - {k} tem o valor {v} anos')
+# print(dados)
