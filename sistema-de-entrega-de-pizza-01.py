ca2d6c30a61a4fbe6b27336267d56c932cf195ae
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-09-15 17:15:56 -0300

    Create sistema-de-entrega-de-pizza-01.py

---
+++ sistema-de-entrega-de-pizza-01.py
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------------------------
+# Fonte:
+# Sistema De (Delivery)Entrega De Pizza Python Tkinter Sqlite3 Com Painel Administrativo - 01
+# https://www.youtube.com/watch?v=y6VX44KU7H4&list=PL2Dw5PtrD32y9o7dm9-hxB366pzNo0Xkk&index=2
+# ------------------------------------------------------------------------------------------------
+
+from tkinter import*
+import time
+import random
+import tikinter import messagebox 
+class Pizza: 
+def main(sf):
+sf.scr.destroy 
+class Pizza:
+def main(sf):
+try:
+sf.scr.destroy()
+sf.scr=Tk()
+except:
+try:
+sf.src=Tk()
+except:
+pass 
+
+
+
+
+
