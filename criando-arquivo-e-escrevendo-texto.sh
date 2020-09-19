67a04c070ca442ec643d3be1ada6638ee71ffc88
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-09-19 13:13:09 -0300

    Update criando-arquivo-e-escrevendo-texto.sh

--- criando-arquivo-e-escrevendo-texto.sh
+++ criando-arquivo-e-escrevendo-texto.sh
@@ -8,16 +8,16 @@
 2) Dentro do diretório mydir, criado na etapa anterior, o shell # script deve imprimir o seu nome e matrícula no arquivo SeuNome.txt 
 (substitua o SeuNome.txt pelo seu próprio nome. Exemplo: se o seu # nome é José, coloque Jose.txt). (2 pontos)
 '''
-$ code atividade2.sh
+$ nano atividade2.sh
 $ sudo chmod +x atividade2.sh
 $ ./atividade2.sh
 Nome: Deise Freire
 echo 'Nome: Deise Freire' > ~/mydir/Deise.txt
-$ code /home/deise/mydir/Deise.txt
+$ nano /home/deise/mydir/Deise.txt
 $ rm ~/mydir/Deise.txt
 echo 'Matrícula: 123456789' 
 $ ./atividade2.sh
-$ code ./home/deise/mydir/Deise.txt
+$ nano ./home/deise/mydir/Deise.txt
 echo 'Matrícula: 123456789' >> ~/mydir/Deise.txt 
 $ ./atividade2.sh
-$ code /home/deise/mydir/Deise.txt
+$ nano /home/deise/mydir/Deise.txt
