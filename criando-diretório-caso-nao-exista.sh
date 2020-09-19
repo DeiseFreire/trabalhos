f5aa5cbea1d8466465b9981573295e99ed5fc62d
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-09-19 10:06:01 -0300

    Update and rename Shell-Script_Atividade-2.sh to criando-diretório-caso-nao-exista.sh

--- criando-diretório-caso-nao-exista.sh
+++ criando-diretório-caso-nao-exista.sh
@@ -1,12 +1,11 @@
 # -------------------------------------------------------------------------------------------------------------------
 # Fonte: 
-# Shell Script - Atividade 2 - Criando arquivo e escrevendo texto
-# https://www.youtube.com/watch?v=mP_I1uR0A70&list=PLh7NspZkGKR-8vj6n1iKi1wtlSMORVgo2&index=2
+# Shell Script - Atividade 1 - Criando diretório caso não exista
+# https://www.youtube.com/watch?v=sMnfPeOZQPc&list=PLh7NspZkGKR-8vj6n1iKi1wtlSMORVgo2&index=1
 # -------------------------------------------------------------------------------------------------------------------
 
 '''
-Atividade: Shell Script Básico
-Escreva um shell script que execute as seguintes tarefas:
+
 1) Verifique se existe o diretório mydir dentro do seu diretório home. Se não existir, crie o diretório. (2 pontos)
 '''
 
