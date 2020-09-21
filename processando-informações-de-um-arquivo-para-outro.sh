2a827e1fdb70fb4744c2d121ebf8946ef14eb529
DeiseFreire <51007898+DeiseFreire@users.noreply.github.com>
2020-09-20 22:38:04 -0300

    Create processando-informações-de-um-arquivo-para-outro.sh

---
+++ processando-informações-de-um-arquivo-para-outro.sh
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------------------------
+# Fonte da ideia:
+# Shell Script - Atividade 4 - Parte 1 - Processando informações de um arquivo para outro
+# https://www.youtube.com/watch?v=198kEfUinG8&list=PLh7NspZkGKR-8vj6n1iKi1wtlSMORVgo2&index=4
+# --------------------------------------------------------------------------------------------
+
+cut -d':' -f1,7 /etc/passwd | grep '/bin/bash' > ~/mydir/Igor.txt
+
+cérebro-binário[09-scripts] $ sudo chmod +x atividade.sh
+cérebro-binário[09-scripts] $ ./atividade4.sh
+cérebro-binário[09-scripts] $ code /home/deise/mydir/Deise.txt
+cérebro-binário[09-scripts] $ ./atividade4.sh
+^Ccérebro-binário[09-scripts] $ ./atividade4.sh
+^Ccérebro-binário[09-scripts] $ ./atividade4.sh
+cérebro-binário[09-scripts] $ code /home/deise/mydir/Deise.txt
+cérebro-binário[09-scripts] $ ./atividade4.sh
+cérebro-binário[09-scripts] $ code /home/deise/mydir/Deise.txt
