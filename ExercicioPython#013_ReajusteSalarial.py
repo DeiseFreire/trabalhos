50b91e67da015a1b25fa98e9c54eb8e8b2c113ab
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-10-30 21:25:19 -0300

    Create ExercicioPython#013_ReajusteSalarial.py

---
+++ ExercicioPython#013_ReajusteSalarial.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------------------
+Fonte da ideia:
+Exercício Python #013 - Reajuste Salarial
+https://www.youtube.com/watch?v=cTkivN8XcJ0&list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-&index=14
+# ------------------------------------------------------------------------------------------
+
+salário = float(input('Qual é o salário do funcionário? R$'))
+novo = salário * 15 / 100   
