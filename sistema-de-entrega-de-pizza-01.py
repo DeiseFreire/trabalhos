206177c56167933f7d88e32ef9b3feb5a8117434
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-11-02 08:11:07 -0300

    Update sistema-de-entrega-de-pizza-01.py

--- sistema-de-entrega-de-pizza-01.py
+++ sistema-de-entrega-de-pizza-01.py
@@ -1,39 +1,41 @@
 # ------------------------------------------------------------------------------------------------
-# Fonte:
+# Fonte da ideia:
 # Sistema De (Delivery)Entrega De Pizza Python Tkinter Sqlite3 Com Painel Administrativo - 01
 # https://www.youtube.com/watch?v=y6VX44KU7H4&list=PL2Dw5PtrD32y9o7dm9-hxB366pzNo0Xkk&index=2
 # ------------------------------------------------------------------------------------------------
 
-from tkinter import*
+from tkinter import *
 import time
 import random
-import tikinter import messagebox 
-class Pizza: 
-def main(sf):
-sf.scr.destroy 
+import tkinter
+import messagebox
+
+
 class Pizza:
-def main(sf):
-try:
-sf.scr.destroy()
-sf.scr=Tk()
+
+
+    def main(sf):
+
+
+    sf.scr.destroy
+
+
+class Pizza:
+
+
+    def main(sf):
+
+
+    try:
+    sf.scr.destroy()
+sf.scr = Tk()
 except:
 try:
-sf.src=Tk()
+    sf.scr = Tk()
 except:
-pass 
-sf.scr.geometry("1366x768")
-sf.scr.title("Pizzaria Sangiorgio: ")
+pass
+sf.scr.geometry( "1366x768" )
+sf.scr.title( "Pizzaria Sangiorgio: " )
 sf.scr.mainloop()
 x = Pizza()
 x.main()
-
-
-
-
-
-
-
-
-
-
-
