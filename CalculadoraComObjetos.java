09479d5f80ab195da8332fd8b726b7d642afb4a8
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-12-11 08:50:18 -0300

    Update CalculadoraComObjetos.java

--- CalculadoraComObjetos.java
+++ CalculadoraComObjetos.java
@@ -3,29 +3,22 @@ import java.util.Scanner;
 
 /*
 * Crie uma classe Calculadora que tenha três * atributos (valor1, valor2, valor3), posteriormente faça o que se pede:
-* 
 * 1.     Crie um construtor para inicializar o atributo valor1
-* 
 * 2.     Crie outro construtor para inicializar os atributos valor1 e valor2;
-* 
 * 3.     Crie um terceiro construtor para inicializar os atributos valor1, valor2 e valor3;
-* 
 * 4.     Crie um método, sem parâmetros, que retorne a soma de todos os atributos.
-* 
 * 5.     Crie um método, sem parâmetros, que retorne a subtração de todos os atributos.
-* 
 * 6.     Crie um método, sem parâmetros, que retorne a divisão de todos os atributos.
-* 
 * 7.     Crie um método, sem parâmetros, que retorne a multiplicação de todos os atributos.
-* 
 * 8.     Implemente o método main para apresentar o contexto referente aos itens acima.
  */
+
 /**
- * Referência bibliográfica: CALCULADORA com orientação à objetos (java).
+ * FONTE DA IDEIA: 
+ * CALCULADORA com orientação à objetos (java).
  * Direção de Willian Barata. Produção de Willian Barata. Brasil: Youtube, 2018.
  * 1 Vídeo (10min.37seg.), color.
  *
- * @author deise
  */
 public class CalculadoraComObjetos {
 
