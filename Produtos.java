806a31ce065f87c1449fdaac887e40c146ab90aa
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-12-11 08:58:02 -0300

    Update Produtos.java

--- Produtos.java
+++ Produtos.java
@@ -1,10 +1,7 @@
 
 /* 
-* 
 * 1 – 4 - LISTA DE EXERCÍCIO 
-*
 * Exercício 4
-*
 * Identifique as classes e implemente um programa para a seguinte 
 * especificação: “O supermercado vende diferentes tipos de produtos. Cada 
 * produto tem um preço e uma quantidade em estoque. Um pedido de um 
@@ -12,12 +9,15 @@
 * cliente deseja e a respectiva quantidade. Esse pedido pode ser pago em 
 * dinheiro, cheque ou cartão.”
 * 
- */
+*/
+
 import javax.swing.JOptionPane;
 
 /**
- *
- * @author deise
+*
+* FONTE DA IDEIA:
+* EXERCÍCIO 01 em java: Simulando um supermercado. Canal: Cursos Wl. 
+* Brasil: Wilker, 2016. Duração (17'43"), color. Youtube.
  */
 public class Produtos {
 
@@ -29,11 +29,18 @@ public class Produtos {
     public void exibirProduto() {
         JOptionPane.showMessageDialog(null, "Nome do produto: " + nome + "\nQuantidade: " + quantidade + "\nUnidade de Medida: " + unidadeMedida + "\nPreço: " + preco);
 
-        /*
-*
-* Referência usada para fazer o exercício:
-* EXERCÍCIO 01 em java: Simulando um supermercado. Canal: Cursos Wl. 
-* Brasil: Wilker, 2016. Duração (17'43"), color. Youtube.
-         */
+    }
+    public class Supermercado {
+
+    public static void main(String[] args) {
+
+        Produtos produto1 = new Produtos();
+        produto1.nome = "Café";
+        produto1.quantidade = 10;
+        produto1.unidadeMedida = "Kg";
+        produto1.preco = 25.00;
+        produto1.exibirProduto();
+
     }
 }
+
