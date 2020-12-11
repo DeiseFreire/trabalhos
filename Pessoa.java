be36fabf7366b4b83ce93fb54f74c96db92b6620
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-12-11 12:21:29 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,11 +1,3 @@
-/*
- * Crie as classes Pessoa e Endereco e faça o que se pede:
- * 1 - Na classe Endereco, insira os atributos logradouro, numero, complemento, bairro, cidade e CEP. Crie um construtor vazio e outro para inicializar todos os atributos da classe.
- * 2 - Na classe Pessoa, insira os atributos nome, CPF, e endereço (associação da classe Endereco). Crie um construtor vazio e outro para inicializar todos os atributos da classe.
- * 3 - Crie um método, em cada classe do exemplo, para retornar uma String que apresente o conteúdo de todos os atributos;
- * 4 - Implemente o método main para apresentar o contexto referente aos itens acima.
- */
-
 /**
  *
  * JAVA: Orientado a objetos classe pessoa. Produção de [s. N.]. [S. L.]: Publicado
