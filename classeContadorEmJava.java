048fe445a64d14f7b173efd0c581a38b17190dc5
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2020-12-20 22:50:53 -0300

    Create classeContadorEmJava.java

---
+++ classeContadorEmJava.java
@@ -0,0 +1,36 @@
+/*
+ * Fonte da ideia:
+ * AULA 37: Classe Contador em Java. Produção de professor Takuno. Publicado pelo canal de professor 
+ * Takuno. [S.l: s.n.], 2017. 1 videoaula. (11 min. 04 segs.). Youtube.
+ */
+
+/**
+ *
+ * @author deise
+ */
+public class ContadorDePessoas {
+
+    public static void main(String[] args) {
+        Contador c1 = new Contador(10);
+
+        c1.incrementar(3);
+        c1.incrementar(3);
+        c1.incrementar(3);
+        c1.incrementar(3);
+        System.out.println(c1.getValor());
+
+    }
+}
+public class ContadorDePessoas {
+
+    public static void main(String[] args) {
+        Contador c1 = new Contador(10);
+
+        c1.incrementar(3);
+        c1.incrementar(3);
+        c1.incrementar(3);
+        c1.incrementar(3);
+        System.out.println(c1.getValor());
+
+    }
+}
