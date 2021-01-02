b2e8a8bf1c83c141c6f18e9c1e55e91e910c4bb1
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-01-02 15:18:28 -0300

    Update ContaBancaria.java

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -3,7 +3,9 @@
  * CURSO de Java - Orientação a Objetos: Correção Exercícios Aula 43 - Parte 1 (Conta Bancária). Produção de Loiane Groner. Publicado pelo canal de Loiane Groner. 
  * [S. l.], 2016. 1 videoaula (31min. 22segs.). Youtube.
  */
+
 package com.deisefreire;
+import java.util.Calendar;
 
 public class ContaBancaria {
     
@@ -56,3 +58,77 @@ public class ContaBancaria {
         return false;
     }
 }
+public class ContaEspecial extends ContaBancaria {
+    
+    private double limite;
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    @Override
+    public String toString() {
+        String s = "ContaEspecial[";
+        s += " limite: " + limite;
+        s += "; " + super.toString(); 
+        s += "]" ;
+        return s; 
+    }
+    
+    public boolean sacar(double valor){
+        
+        double saldoComLimite = this.getSaldo() + limite;
+        
+        if ((saldoComLimite-valor) >=0){
+            this.setSaldo(this.getSaldo()-valor);
+            
+            //super.sacar(valor);
+            return true;
+        }
+        
+        return false;
+    }
+}
+public class ContaPoupanca {
+
+    public class ContaPoupanca extends ContaBancaria {
+
+        private int diaRendimento;
+
+        public int getDiaRendimento() {
+            return diaRendimento;
+        }
+
+        public void setDiaRendimento(int diaRendimento) {
+            this.diaRendimento = diaRendimento;
+        }
+
+        @Override
+        public String toString() {
+            String s = "ContaPoupanca [";
+            s += " di: " + diaRendimento;
+            s += "numConta: " + numConta;
+            s += super.toString();
+            s += "]";
+            return s;
+
+        }
+
+        public void calcularNovoSaldo(double taxaRendimento) {
+            Calendar hoje = Calendar.get.Instance();
+            if (diaRendimento == hoje.get(Calendar.DAY_OF_MONTH)) {
+                saldo += saldo + taxaRendimento;
+                this.setSaldo(this.getSaldo() + (this.getSaldo() * taxaRendimento));
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
+
+
