c3183e3075c7bafa1d474a0509618641bddfe9f7
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-01-07 18:15:22 -0300

    Create EncapsulamentoeHerança_quiz.java

---
+++ EncapsulamentoeHerança_quiz.java
@@ -0,0 +1,42 @@
+Uma classe bem definida deve separar completamente sua interface pública da implementação interna		
+Consiste em esconder dados da classe deixando-as apenas disponíveis através de metdos		
+Aplica-se utilizando modificadores de acesso nos membros da classe.		
+O modificador de acesso padrão é o internalize.	[true]	
+
+
+O modificador de acesso protected é um empregado em um atributo/método acessado somente por membros da própria classe. [false]
+
+O modificador de acesso private é empregado por um atributo/método que pode ser acessado por objetos de subclasse e que estiverem no mesmo pacote. [false]
+
+
+O modificador de acesso public pode ser empregado por um atributo/método que pode ser acessado por outras classes. [true]
+
+Preencha as palavras que faltam com GET ou SET (A resposta deve ser preenchida em letra maiuscula)
+
+[SET] - Um método sem retorno (void) e com parâmetros que altera o valor de um mais atributos
+  
+[GET] - Um método sem parâmetros e com tipo de retorno definido que devolve o valor de determinado atributo.
+
+A palavra reservada THIS é usado normalmente quando: 
+1 - O nome de uma variável de instância é igual ao nome de um parâmetro do método; 
+2 - O método precisa chamar outro método passando por parâmetro uma referência ao objeto atual. [true]
+
+
+Herança é um mecanismo de reutilização de software onde uma nova classe é criada absorvendo membros de uma classe existente e aprimorada com capacidades novas ou modificadas. [true]
+
+
+Arraste as palavras aos campos corretos
+A Superclasse caracteriza-se por:
+
+1 - Mais geral
+
+2 - Superclasse direta: herança explícita;
+
+3 - Superclasse indireta: qualquer superclasse acima da classe direta na hierarquia de classes
+
+A subclasse é definida por:
+
+1 - Mais específica;
+
+2 - Pode se tornar uma superclasse para novas subclasses
+
