de11243b6d81b491118f59a71d62c4db50c8efb4
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-01-24 23:49:49 -0300

    Create questionarioavaliativo5.bimestre.exer01.java

---
+++ questionarioavaliativo5.bimestre.exer01.java
@@ -0,0 +1,143 @@
+package com.deise.questionarioavaliativo5.bimestre.exer01;
+
+/**
+ *
+ * @author deise
+ */
+
+public class Bimestre {
+
+    Bimestre(Avaliacao av1, Avaliacao av2, Avaliacao av3) {
+
+        this.av1 = av1;
+        this.av2 = av2;
+        this.av3 = av3;
+    }
+    private Avaliacao av1;
+    private Avaliacao av2;
+    private final Avaliacao av3;
+    public void imprimirAvaliacoes() {
+        System.out.println(av1.getInfo());
+        av1.dados();
+        System.out.println(av2.getInfo());
+        av2.dados();
+        System.out.println(av3.getInfo());
+        av3.dados();
+    }
+}
+package com.deise.questionarioavaliativo5.bimestre.exer01;
+
+/**
+ *
+ * @author deise
+ */
+
+public abstract class Avaliacao {
+    private String nome;
+    private double nota;
+    public Avaliacao(String nome, Double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+    ;
+    public abstract void dados();
+    public String getInfo() {
+        return nome;
+    }
+    public double getNota() {
+        return nota;
+    }
+}
+package com.deise.questionarioavaliativo5.bimestre.exer01;
+
+/**
+ *
+ * @author deise
+ */
+
+public class Prova extends Avaliacao {
+    private Double tempoDuracao;
+    private int quantidadeDeQuestoes;
+    private boolean consultaAutorizada;
+    public Prova(String nome, Double nota, double tempo, int qtdQuestoes, boolean consulta) {
+        super(nome, nota);
+        tempoDuracao = tempo;
+        quantidadeDeQuestoes = qtdQuestoes;
+        consultaAutorizada = consulta;
+    }
+    @Override
+    public void dados() {
+        System.out.print(" Prova: Nota: " + getNota());
+        System.out.print(" Tempo: " + tempoDuracao);
+        System.out.print(" Quantidade de Questões: " + quantidadeDeQuestoes);
+        System.out.println(" Consulta: " + consultaAutorizada);
+    }
+}
+
+package com.deise.questionarioavaliativo5.bimestre.exer01;
+
+/**
+ *
+ * @author deise
+ */
+
+public class Seminario extends Avaliacao {
+    private String tema;
+    private Double tempoDuracao;
+    public Seminario(String nome, Double nota, Double tempo, String tema) {
+        super(nome, nota);
+        this.tema = tema;
+        tempoDuracao = tempo;
+    }
+    @Override
+    public void dados() {
+        System.out.print(" Seminário: Nota: " + getNota());
+        System.out.print(" Tema: " + tema);
+        System.out.println(" Tempo: " + tempoDuracao);
+    }
+}
+
+package com.deise.questionarioavaliativo5.bimestre.exer01;
+
+/**
+ *
+ * @author deise
+ */
+
+public class EstudoDirigido extends Avaliacao {
+    private String tema;
+    private int numeroDePaginas;
+    public EstudoDirigido(String nome, Double nota, int numPaginas, String tema) {
+        super(nome, nota);
+        numeroDePaginas = numPaginas;
+        this.tema = tema;
+    }
+    @Override
+    public void dados() {
+        System.out.print(" Estudo Dirigido: Nota: " + getNota());
+        System.out.print(" Tema: " + tema);
+        System.out.println(" Numero de Páginas: " + numeroDePaginas);
+    }
+
+}
+
+package com.deise.questionarioavaliativo5.bimestre.exer01;
+
+/**
+ *
+ * @author deise
+ */
+
+public class BimestreTesteMain {
+    public static void main(String[] args) {
+
+        Prova av1 = new Prova("maria", 10.4, 300.0, 14, false);
+        Seminario av2 = new Seminario("maria", 45.9, 400.0, "Historia da Educação");
+        EstudoDirigido av3 = new EstudoDirigido("Jose", 9.4, 20, "Brazza");
+        Bimestre a = new Bimestre(av1, av2, av3);
+        a.imprimirAvaliacoes();
+
+    }
+
+}
+
